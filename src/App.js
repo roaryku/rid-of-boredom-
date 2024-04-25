079b07d15cf3445cb9c1fdeb672203708d6883ac
Rodika Aryku <raryku@yahoo.com>
2024-04-24 18:41:40 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import video from './tips.mp4'
 import './App.css';
 
 function App() {
+ 
+  const [tips, setTips] = useState("");
+
+  useEffect(() => {
+    fetchTips()
+  }, [])
+
+  const fetchTips = async() => {
+    const responce = await fetch("http://www.boredapi.com/api/activity/");
+    const data = await responce.json();
+    // console.log(data.activity)
+    setTips(data.activity)
+  }
+  // fetchTips();
+
   return (
+  <div>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Are You Bored 🙄 ?</h1>
+    <h3>Here are some tips how to get rid of boredom</h3>
+    {/* <img src={icon} alt='icon' width='200'/> */}
+    <video autoPlay muted loop>
+            <source src={video} type='video/mp4'/>
+          </video>
+    <p>{tips}</p>
+    <button onClick={fetchTips}>New Tip</button>
+     </div>
+   </div>
   );
 }
 
